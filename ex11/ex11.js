bd6e996165d6b3b72a751a6ac678f7b0c2a42a95
André Lucas Junqueira <130708772+AndreLJunqueira@users.noreply.github.com>
2023-07-11 16:04:08 -0300

    Estrutura de repetição

---
+++ ex11/ex11.js
@@ -0,0 +1,12 @@
+var c = 1
+while (c <= 6) {
+    console.log(`passo ${c}`)
+    c++ // c = c + 1
+} //teste lógico no início
+
+var cont = 1
+do {
+    console.log(`passo ${cont}`)
+    cont++
+} while (cont <= 6)
+// teste lógico no final
